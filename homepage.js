0b3d36527241894936a7fc49b610ed07588e5769
Steve Burnette <sburnet1@hawk.iit.edu>
2016-03-02 05:22:40 -0600

    was able to get videos embeded and formated correctly

---
+++ homepage.js
@@ -0,0 +1,9 @@
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == null || x == "") {
+        alert("All Fields Must Be Completed");
+        return false;
+    }
+}
+
+
